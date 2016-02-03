368288d9400acf1865c225707dbcef929b98fc26
Nathan Hazout <nathanh@il.ibm.com>
2016-02-03 12:32:38 +0200

    get balance

--- PinCodeSwift/ViewController.swift
+++ PinCodeSwift/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import IBMMobileFirstPlatformFoundation
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func getBalance(sender: UIButton) {
+        let request = WLResourceRequest(URL: NSURL(string: "/adapters/ResourceAdapter/balance"), method: WLHttpMethodGet)
+        request.sendWithCompletionHandler { (response, error) -> Void in
+            if(error == nil){
+                NSLog(response.responseText)
+                self.alert("Success", msg: "Balance = " + response.responseText)
+                
+            }
+            else{
+                NSLog(error.description)
+                self.alert("Failure", msg: error.description)
+            }
+        }
+        
+    }
+    
+    func alert(alertTitle: String, msg:String){
+        let alert = UIAlertController(title: alertTitle, message: msg, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK",
+            style: .Default,
+            handler: nil))
+        self.presentViewController(alert,
+            animated: true,
+            completion: nil)
+        
+    }
 
 }
 
