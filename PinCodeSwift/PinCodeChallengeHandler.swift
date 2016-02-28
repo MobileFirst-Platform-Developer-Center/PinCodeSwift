914cd190fd0302071a00e2941594982ebbbd350a
Nathan Hazout <nathanh@il.ibm.com>
2016-02-28 09:17:03 +0200

    Updated licenses

--- PinCodeSwift/PinCodeChallengeHandler.swift
+++ PinCodeSwift/PinCodeChallengeHandler.swift
@@ -1,30 +1,38 @@
-//
-//  PinCodeChallengeHandler.swift
-//  PinCodeSwift
-//
-//  Created by Nathan Hazout on 03/02/2016.
-//  Copyright © 2016 Sample. All rights reserved.
-//
+/**
+* Copyright 2016 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 import Foundation
 import IBMMobileFirstPlatformFoundation
 
 class PinCodeChallengeHandler : WLChallengeHandler {
-    
+
     //SecurityCheck name
     static let securityCheck = "PinCodeAttempts"
-    
+
     //Register this class as a challenge handler
     static func registerSelf() {
         WLClient.sharedInstance().registerChallengeHandler(PinCodeChallengeHandler(securityCheck: securityCheck))
     }
-    
-    
+
+
     //Get this registered challenge handler
     internal static func getRegisteredChallengeHandler()->PinCodeChallengeHandler? {
         return WLClient.sharedInstance().getChallengeHandlerBySecurityCheck(securityCheck) as? PinCodeChallengeHandler
     }
-    
+
     override func handleChallenge(challenge: [NSObject : AnyObject]!) {
         NSLog("%@",challenge)
         var errorMsg : String
@@ -35,10 +43,10 @@ class PinCodeChallengeHandler : WLChallengeHandler {
             errorMsg = challenge["errorMsg"] as! String
         }
         let remainingAttempts = challenge["remainingAttempts"] as! Int
-        
+
         showPopup(errorMsg,remainingAttempts: remainingAttempts)
     }
-    
+
     override func handleFailure(failure: [NSObject : AnyObject]!) {
         if let errorMsg = failure["failure"] as? String {
             showError(errorMsg)
@@ -47,10 +55,10 @@ class PinCodeChallengeHandler : WLChallengeHandler {
             showError("Unknown error")
         }
     }
-    
+
     func showPopup(errorMsg: String, remainingAttempts: Int){
         let message = errorMsg + "\nRemaining attempts: " + String(remainingAttempts)
-        
+
         let alert = UIAlertController(title: "Protected",
             message: message,
             preferredStyle: .Alert)
@@ -65,23 +73,23 @@ class PinCodeChallengeHandler : WLChallengeHandler {
         alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { (action) -> Void in
             self.submitFailure(nil)
         }))
-        
+
         dispatch_async(dispatch_get_main_queue()) {
             let topController = UIApplication.sharedApplication().keyWindow!.rootViewController! as UIViewController
             topController.presentViewController(alert,
                 animated: true,
                 completion: nil)
         }
-        
-        
+
+
     }
-    
+
     func showError(errorMsg: String){
         let alert = UIAlertController(title: "Error",
             message: errorMsg,
             preferredStyle: .Alert)
         alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-        
+
         dispatch_async(dispatch_get_main_queue()) {
             let topController = UIApplication.sharedApplication().keyWindow!.rootViewController! as UIViewController
             topController.presentViewController(alert,
@@ -89,9 +97,9 @@ class PinCodeChallengeHandler : WLChallengeHandler {
                 completion: nil)
         }
     }
-    
+
     override func handleSuccess(success: [NSObject : AnyObject]!) {
         NSLog("handleSuccess: %@",success)
     }
-    
+
 }
